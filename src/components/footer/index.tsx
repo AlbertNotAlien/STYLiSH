d0eb9e6a4b413b4eeb1c6d34c3b70c2aab45535d
AlbertNotAlien <albert830207@gmail.com>
2023-08-24 00:17:49 +0800

    feat: Completed footer layout in desktop size

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import lineIcon from "@/public/images/icon/social-media/line.png";
+import twitterIcon from "@/public/images/icon/social-media/twitter.png";
+import facebookIcon from "@/public/images/icon/social-media/facebook.png";
+
+const FOOTER_NAV_LINKS = [
+  {
+    title: "關於 STYLiSH",
+    url: "/",
+  },
+  {
+    title: "服務條款",
+    url: "/",
+  },
+  {
+    title: "隱私政策",
+    url: "/",
+  },
+  {
+    title: "聯絡我們",
+    url: "/",
+  },
+  {
+    title: "FAQ",
+    url: "/",
+  },
+];
+
+const SOCIAL_MEDIA_ICONS = [
+  {
+    alternative: "line-icon",
+    src: lineIcon,
+    url: "/",
+  },
+  {
+    alternative: "twitter-icon",
+    src: twitterIcon,
+    url: "/",
+  },
+  {
+    alternative: "facebook-icon",
+    src: facebookIcon,
+    url: "/",
+  },
+];
+
+export default function Footer() {
+  return (
+    <div className="flex h-[115px] w-full justify-center bg-background-gray">
+      <div className="flex w-page-container max-w-page-container items-center justify-between gap-x-[30px]">
+        <div className="flex grow items-center justify-between">
+          <nav>
+            <li className="divide-text-dark-gray flex divide-x">
+              {FOOTER_NAV_LINKS.map((link) => (
+                <ul className="flex h-4 w-[134px] justify-center text-center">
+                  <Link
+                    className="text-text-white text-base leading-4"
+                    href={link.url}
+                  >
+                    {link.title}
+                  </Link>
+                </ul>
+              ))}
+            </li>
+          </nav>
+          <li className="flex gap-x-[30px]">
+            {SOCIAL_MEDIA_ICONS.map((socialMedia) => (
+              <ul key={socialMedia.alternative}>
+                <Link href={socialMedia.url}>
+                  <Image
+                    src={socialMedia.src}
+                    alt={socialMedia.alternative}
+                    width={50}
+                    height={50}
+                    className="min-w-[50px]"
+                  />
+                </Link>
+              </ul>
+            ))}
+          </li>
+        </div>
+        <p className="text-text-dark-gray grow text-xs">
+          © 2023. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+}
