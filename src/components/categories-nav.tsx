3c295ec659342508cec9c94a69fd0cdc2a0d725f
AlbertNotAlien <albert830207@gmail.com>
2023-09-11 01:49:28 +0800

    feat: Pushed the queryString to the router upon clicking on a category

--- src/components/categories-nav.tsx
+++ src/components/categories-nav.tsx
@@ -1,40 +1,62 @@
-import React from 'react';
-import Link from 'next/link';
+'use client';
 
-const NAV_LINK_LIST = [
+import React, { useState, useCallback } from 'react';
+import { useRouter, useSearchParams, usePathname } from 'next/navigation';
+
+const CATEGORY_LIST = [
   {
     title: '女裝',
-    url: '/',
+    category: 'women',
   },
   {
     title: '男裝',
-    url: '/',
+    category: 'men',
   },
   {
     title: '配件',
-    url: '/',
+    category: 'accessories',
   },
 ];
 
-// TODO: #808080
 // TODO: CategoriesNav links onClick padding
 export default function CategoriesNav() {
-  const selectedIndex = 0;
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const categoryParams = CATEGORY_LIST.find((category) => searchParams.get('category') === category.category)?.category || 'all';
+  const [category, setCategory] = useState(categoryParams);
+
+  const createQueryString = useCallback(
+    // TODO: maybe move to utils
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  const selectCategory = (newCategory: string) => {
+    setCategory(newCategory);
+    router.push(`${pathname}?${createQueryString('category', newCategory)}`);
+  };
+
   return (
     <nav
       role="list"
-      className="col xl:divide-stylish-gray-darker flex h-full w-full items-center divide-x-[1px] divide-[#808080]"
+      className="col xl:divide-stylish-gray-darker flex h-full w-full items-center divide-x-[1px] divide-stylish-gray-medium"
     >
-      {NAV_LINK_LIST.map((category, index) => (
+      {CATEGORY_LIST.map((item) => (
         <div
-          key={category.title}
+          key={item.title}
           className={`w-full text-center text-base xl:w-[150px] xl:indent-[30px] xl:text-xl xl:leading-5 xl:tracking-[30px] ${
-            selectedIndex === index
+            category === item.category
               ? 'text-stylish-white xl:text-stylish-gold'
               : 'text-stylish-gray-medium xl:text-stylish-gray-darker'
           }`}
         >
-          <Link href={category.url}>{category.title}</Link>
+          <button type="submit" onClick={() => selectCategory(item.category)}>{item.title}</button>
         </div>
       ))}
     </nav>
