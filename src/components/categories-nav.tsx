a36ce4db1d0e1f05510a807712f1d19f38379768
AlbertNotAlien <albert830207@gmail.com>
2023-09-25 01:47:01 +0800

    feat: Added outline style for search-bar when it is active

--- src/components/categories-nav.tsx
+++ src/components/categories-nav.tsx
@@ -23,18 +23,21 @@ const CATEGORY_LIST = [
 export default function CategoriesNav() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const categoryParams = CATEGORY_LIST.find((category) => searchParams.get('category') === category.category)?.category || 'all';
+  const categoryParams =
+    CATEGORY_LIST.find(
+      (category) => searchParams.get('category') === category.category,
+    )?.category || 'all';
 
   return (
     <nav
       role="list"
-      className="col xl:divide-stylish-gray-darker flex h-full w-full items-center divide-x-[1px] divide-stylish-gray-medium"
+      className="col flex h-full w-full items-center divide-x-[1px] divide-stylish-gray-medium xl:divide-stylish-gray-darker"
     >
       {CATEGORY_LIST.map((item) => (
         <Link
           key={item.title}
           href={`${pathname}?category=${item.category}`}
-          className={`w-full text-center text-base xl:w-[150px] xl:indent-[30px] xl:text-xl xl:leading-5 xl:tracking-[30px] ${
+          className={`w-full text-center text-base hover:brightness-125 xl:w-[150px] xl:indent-[30px] xl:text-xl xl:leading-5 xl:tracking-[30px] ${
             categoryParams === item.category
               ? 'text-stylish-white xl:text-stylish-gold'
               : 'text-stylish-gray-medium xl:text-stylish-gray-darker'
