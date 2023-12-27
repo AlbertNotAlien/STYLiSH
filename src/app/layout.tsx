7d078daaf7f31dc296367cd69044e2e3e9b6acc0
AlbertNotAlien <albert830207@gmail.com>
2023-12-28 02:35:16 +0800

    refactor: Refactored Header and Footer structure in the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <>
+          <Header />
+          <main className="mb-[96px] mt-[140px]">{children}</main>
+          <Footer />
+        </>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,12 @@
 import React from "react";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
 import Carousel from "./home/carousel";
 import Products from "./home/products";
 
-// TODO: header, main, footer
 export default function Home() {
   return (
     <>
-      <Header />
-      <main className="mb-[96px] mt-[140px]">
-        <Carousel />
-        <Products />
-      </main>
-      <Footer />
+      <Carousel />
+      <Products />
     </>
   );
 }
