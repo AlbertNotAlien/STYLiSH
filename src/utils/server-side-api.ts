18cfda8c034b70591acffe7e06de98a13527f34f
AlbertNotAlien <albert830207@gmail.com>
2023-12-28 02:35:16 +0800

    feat: Added error handle in server-side-api

--- src/utils/server-side-api.ts
+++ src/utils/server-side-api.ts
@@ -4,6 +4,12 @@ export const fetchAllProducts = async ({ category = 'all', paging = 0 }) => {
   const res = await fetch(
     `https://${API_HOST_NAME}/api/${API_VERSION}/products/${category}?paging=${paging}`,
   );
+
+  if (!res.ok) {
+    // TODO: This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
   return res.json();
 };
 
