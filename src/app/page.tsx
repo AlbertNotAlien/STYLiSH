691d9d34da74faa045dedb68a42da087920d0640
AlbertNotAlien <albert830207@gmail.com>
2023-12-28 02:35:16 +0800

    refactor: Formatted home with prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,21 @@ import Carousel from './home/carousel';
 import Products from './home/products';
 import { ProductsType } from '@/types/products';
 
-export default async function Home(
-  { searchParams }: { searchParams?: { [key: string]: string | string[] | undefined }; },
-) {
-  const categoryParams = searchParams && searchParams.category && typeof searchParams.category === 'string' ? searchParams.category : 'all';
-  const products: ProductsType = await fetchAllProducts({ category: categoryParams });
+// export const dynamic = 'force-dynamic';
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  const categoryParams = searchParams
+    && searchParams.category
+    && typeof searchParams.category === 'string'
+    ? searchParams.category
+    : 'all';
+  const products: ProductsType = await fetchAllProducts({
+    category: categoryParams,
+  });
   return (
     <>
       <Carousel />
