d8e0f99dc3a1ee0ce0cec0fd0a2074d4ef399f06
AlbertNotAlien <albert830207@gmail.com>
2023-12-28 02:35:16 +0800

    refactor: Changed category link from button to Link

--- src/components/categories-nav.tsx
+++ src/components/categories-nav.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import React, { useState, useCallback } from 'react';
-import { useRouter, useSearchParams, usePathname } from 'next/navigation';
+import React from 'react';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 
 const CATEGORY_LIST = [
   {
@@ -20,27 +21,27 @@ const CATEGORY_LIST = [
 
 // TODO: CategoriesNav links onClick padding
 export default function CategoriesNav() {
-  const router = useRouter();
-  const pathname = usePathname();
+  // const router = useRouter();
+  // const pathname = usePathname();
   const searchParams = useSearchParams();
   const categoryParams = CATEGORY_LIST.find((category) => searchParams.get('category') === category.category)?.category || 'all';
-  const [category, setCategory] = useState(categoryParams);
+  // const [category, setCategory] = useState(categoryParams);
 
-  const createQueryString = useCallback(
-    // TODO: maybe move to utils
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams);
-      params.set(name, value);
+  // const createQueryString = useCallback(
+  //   // TODO: maybe move to utils
+  //   (name: string, value: string) => {
+  //     const params = new URLSearchParams(searchParams);
+  //     params.set(name, value);
 
-      return params.toString();
-    },
-    [searchParams],
-  );
+  //     return params.toString();
+  //   },
+  //   [searchParams],
+  // );
 
-  const selectCategory = (newCategory: string) => {
-    setCategory(newCategory);
-    router.push(`${pathname}?${createQueryString('category', newCategory)}`);
-  };
+  // const selectCategory = (newCategory: string) => {
+  //   setCategory(newCategory);
+  //   router.push(`${pathname}?${createQueryString('category', newCategory)}`);
+  // };
 
   return (
     <nav
@@ -48,16 +49,17 @@ export default function CategoriesNav() {
       className="col xl:divide-stylish-gray-darker flex h-full w-full items-center divide-x-[1px] divide-stylish-gray-medium"
     >
       {CATEGORY_LIST.map((item) => (
-        <div
+        <Link
           key={item.title}
+          href={`?category=${item.category}`}
           className={`w-full text-center text-base xl:w-[150px] xl:indent-[30px] xl:text-xl xl:leading-5 xl:tracking-[30px] ${
-            category === item.category
+            categoryParams === item.category
               ? 'text-stylish-white xl:text-stylish-gold'
               : 'text-stylish-gray-medium xl:text-stylish-gray-darker'
           }`}
         >
-          <button type="submit" onClick={() => selectCategory(item.category)}>{item.title}</button>
-        </div>
+          {item.title}
+        </Link>
       ))}
     </nav>
   );
